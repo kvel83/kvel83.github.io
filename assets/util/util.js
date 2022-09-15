8b8b1de5c56262fd4efdb753886dd5fbbe411110
Katia Velasquez <katia.velasquez@.com>
2022-09-14 23:55:30 -0300

    se agrega archivo util.js para manejo dinamico de modal, se agrega modal, se agrega seccion conocimientos y proyectos

---
+++ assets/util/util.js
@@ -0,0 +1,23 @@
+/* VALIDACIÓN INPUT */
+function proyectDetail(proyectName) {
+    /* var nombre = document.getElementById('inputName');
+    var mail = document.getElementById('userMail'); */
+    const modal = document.getElementById('comunModal');
+    const modalTitle = modal.querySelector('.modal-title');
+    const modalBodyInput = modal.querySelector('.modal-body p');
+    const modalImage=modal.querySelector('.modal-body img');
+    const modalFooter=modal.querySelector('.modal-footer');
+   /*  const modalButton=modal.querySelector() */
+    /* Validacion Nombre */
+    if (proyectName.id=="cv") {
+        modalTitle.textContent = "Currículum Vitae";
+        modalBodyInput.textContent = "Primer proyecto para bootcamp Desarrollo FullStack JavaScript realizado con HTML, CSS y Bootstrap ";
+        modalImage.src="assets/img/cv.png";
+        modalImage.className="img-fluid pb-2";
+        modalFooter.children[0].href="https://github.com/kvel83/Ejercicios-Desafio-Latam/tree/main/Proyecto1(CV)";
+        return false;
+      
+    }
+    modalTitle.textContent = "CUPPON";
+    modalBodyInput.textContent = "Gracias por su registro";
+}
