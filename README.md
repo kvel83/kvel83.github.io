99633cb61e693c301fa84985cffb061f1665cf86
Katia Velasquez <katia.velasquez@.com>
2022-09-18 17:34:19 -0300

    Se agrega area de text a formulario, se suben archivos de proyectos y se agregan links

--- README.md
+++ README.md
@@ -7,27 +7,29 @@ En este examen se evaluará el conocimiento y dominio de HTML, CSS, Bootstrap, G
 
 También se podrá apreciar el avance de conocimientos obtenidos ya que se tendrá acceso a los repositorios de los desafíos anteriores.
 
+Link para acceder a página del portafolio: https://kvel83.github.io
+
 ### DESAFÍOS PRIMER MÓDULO
 
-- Currículum Vitae
+- Currículum Vitae (https://github.com/kvel83/proyectosDesafioLatam/tree/main/Proyecto1(CV))
 
   ![](assets/img/cv.png)
 
 Pagina desarrollada con HTML, CSS y Bootstrap. Primer desafío del módulo donde debiamos demostrar el manejo de HTML realizando un correcto uso de las etiquetas básicas y semánticas así como el uso de buenas prácticas de 	             programación.
 
-- Iguana Page
+- Iguana Page(https://github.com/kvel83/proyectosDesafioLatam/tree/main/Proyecto3(Iguana%20Page))
 
   ![](assets/img/iguana.png)
 
 Pagina desarrollada con HTML y CSS . Segundo desafío del módulo donde debíamos demostrar el conocimiento y manejo de de posicionamiento utilizando la propiedad display y haciendo uso de clases y modelo de cajas para aplicar los formatos indicados. También se evalúa la capacidad de réplicar la maqueta entregada como requerimiento y se evalúa el correcto uso de las etiquetas HTML y de buenas prácticas de programación.
 
-- Cuppon
+- Cuppon(https://github.com/kvel83/proyectosDesafioLatam/tree/main/Proyecto4(Cuppon))
 
   ![](assets/img/cuppon.png)
 
 Página desarrollada con HTML, CSS y Bootstrap. Tercer desafío del módulo  donde debíamos realizar un uso al máximo de Bootstrap y reducir al mínimo el uso de CSS. En esta página apliqué el modelo "Mobile First" haciendola 	100% responsiva, implementé modales dinámicos y manejé con JS la validación del formulario de contacto y la información mostrada en los modales.
 
-- Página Personal
+- Página Personal(https://github.com/kvel83/fdsw-github)
 
 ![](assets/img/plantilla.jpg)
 
--- assets/util/util.js
+++ assets/util/util.js
@@ -10,7 +10,7 @@ function proyectDetail(proyectName) {
         modalBodyInput.textContent = "Primer proyecto para bootcamp Desarrollo FullStack JavaScript realizado con HTML, CSS y Bootstrap aplicando buenas prácticas de codificación y etiquetas semánticas para una correcta interpretación y revisión del código fuente. ";
         modalImage.src="assets/img/cv.png";
         modalImage.className="img-fluid pb-2";
-        modalFooter.children.repositorio.href="https://github.com/kvel83/Ejercicios-Desafio-Latam/tree/main/Proyecto1(CV)";
+        modalFooter.children.repositorio.href="https://github.com/kvel83/proyectosDesafioLatam/tree/main/Proyecto1(CV)";
         modalFooter.children.repositorio.target="_blank"
         return false;
 
@@ -20,7 +20,7 @@ function proyectDetail(proyectName) {
         modalBodyInput.textContent = "Segundo proyecto para bootcamp Desarrollo FullStack JavaScript realizado con HTML y CSS aplicando conocimientos de posicionamiento utilizando la propiedad display y haciendo uso de clases y modelo de cajas para aplicar los formatos requeridos para cumplir con la maqueta entregada y que se debía replicar.";
         modalImage.src="assets/img/iguana.png";
         modalImage.className="img-fluid pb-2";
-        modalFooter.children.repositorio.href="https://github.com/kvel83/Ejercicios-Desafio-Latam/tree/main/Proyecto3(Iguana%20Page)";
+        modalFooter.children.repositorio.href="https://github.com/kvel83/proyectosDesafioLatam/tree/main/Proyecto3(Iguana%20Page)";
         modalFooter.children.repositorio.target="_blank"
         return false;
 
@@ -30,7 +30,7 @@ function proyectDetail(proyectName) {
         modalBodyInput.textContent = "Tercer proyecto para bootcamp Desarrollo FullStack JavaScript realizado con HTML, CSS y Bootstrap donde la codificación de CSS se redujo al máximo usandose sólo para las propiedades que no se podían manejar con Bootstrap directamente. Se aplica diseño 'Mobile First' haciendo la página totalmente responsiva y se implementa el uso de Modal dinámico y JS para validación de formulario de contacto.";
         modalImage.src="assets/img/cuppon.png";
         modalImage.className="img-fluid pb-2";
-        modalFooter.children.repositorio.href="";
+        modalFooter.children.repositorio.href="https://github.com/kvel83/proyectosDesafioLatam/tree/main/Proyecto4(Cuppon)";
         modalFooter.children.repositorio.target="_blank"
         return false;
 
@@ -50,6 +50,7 @@ function proyectDetail(proyectName) {
 function validateInput() {
     var nombre = document.getElementById('inputName');
     var mail = document.getElementById('userMail');
+    var message=document.getElementById('userMessage');
     const modal = document.getElementById('contactModal');
     const modalTitle = modal.querySelector('.modal-title');
     const modalBodyInput = modal.querySelector('.modal-body p');
@@ -64,6 +65,13 @@ function validateInput() {
 
         modalTitle.textContent = "¡Falta tu mail!";
         modalBodyInput.textContent = "Por favor ingresa tu mail!!!!";
+        return false;
+    }
+    /* Validacion Message */
+    if (message.value.length == 0) {
+
+        modalTitle.textContent = "¡Falta el mensaje!";
+        modalBodyInput.textContent = "Por favor ingresa el mensaje que quieres dirigirme.";
         return false;
     }
     modalTitle.textContent = "ENVIO EXITOSO";
